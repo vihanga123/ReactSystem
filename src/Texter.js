d304e56195f5a7257d66658b144c27cbc66dacac
Vihanga <53087925+vihanga123@users.noreply.github.com>
2023-04-06 17:07:27 +0530

    Ex-1

---
+++ src/Texter.js
@@ -0,0 +1,12 @@
+function set() {
+  return(
+  <div>
+  <img src= "./React.png" width="40px"/>
+  <h1>Fun facts about React</h1>
+  <ol>
+    <li>It is better</li>
+    <li>I really don't know</li>
+  </ol>
+</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
+
 import ReactDOM from 'react-dom';
 
-// const hi = document.createElement("hi")
-// hi.textContent="Hello World"
-// hi.className="header"
-// console.log(hi)
+function Set(){
+  return(
+      <header>
+        <nav className='nav'>
+          <img src='./React.png' className='logo'></img>
+          <ul className='nav-items'>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+  )
+}
 
-const nav = (
-  <nav>
-    <h1>Website</h1>
-    <ul>
-      <li>Pricing</li>
-      <li>About</li>
-      <li>Contacts</li>
-    </ul>
-  </nav>
-)
-ReactDOM.render(nav,document.getElementById("root"))
+function Body(){
+  return(
+    <div>
+      <h1>This is the Header</h1>
+      <p>This is a paragraph of the website</p>
+    </div>
+  )
+}
+
+function Footer(){
+  return(
+      <footer className='footer'>
+        Copyright By your mama KEKW
+      </footer>
+  )
+}
+
+function Page(){
+  return(
+    <div className='font'>
+      <Set/>
+      <Body/>
+      <Footer/>
+    </div>
+
+  )
+}
+
+ReactDOM.render(<Page/>,document.getElementById("root"))
+
